19e698cdb4f51b708be5dd232faec649706c7364
Jack Hardcastle <jwhardcastle@jwhardcastle.com>
2014-05-08 22:06:17 -0400

    Error handling with panic, instead of if

--- root/root.go
+++ root/root.go
@@ -6,7 +6,6 @@ import (
 	"fmt"
 	"html/template"
 	"net/http"
-	//"errors"
 )
 
 // Information we feed to the render template
@@ -28,43 +27,47 @@ func init() {
 }
 
 // http.Handle doesn't expect you to return an error, but we want to surface them
-type appHandler func(http.ResponseWriter, *http.Request) *appError
+type appHandler func(http.ResponseWriter, *http.Request)
 
-// Serve HTTP, and handle error messages with dignity, by displaying a nice error page
+// Serve HTTP, and handle error messages with dignity by displaying a nice error page
 func (fn appHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	if e := fn(w, r); e != nil { // e is *appError, not os.Error.
-		c := appengine.NewContext(r)
-		c.Errorf("%v", e.Error)
+	defer func() {
+		if recv := recover(); recv != nil {
+			e := recv.(*appError)
+			c := appengine.NewContext(r)
+			c.Errorf("%v", e.Error)
 
-		http.StatusText(e.Code)
-		t, err := template.ParseFiles("errors/500.html")
-		if err != nil {
-			http.Error(w, "A very serious error has occurred.", 500)
-		}
-		err = t.Execute(w, e)
-		if err != nil {
-			http.Error(w, "A very serious error has occurred.", 500)
-		}
-	}
+			http.StatusText(e.Code)
+			t, err := template.ParseFiles("errors/500.html")
+			if err != nil {
+				http.Error(w, "A very serious error has occurred.", 500)
+			}
+			err = t.Execute(w, e)
+			if err != nil {
+				http.Error(w, "A very serious error has occurred.", 500)
+			}
+			
+		}  
+	} ()
+	fn(w,r)
 }
 
+// Check errors, panic if we have an error
+func check(err error, message string) { if err != nil { panic(&appError{err, message, 500} ) } }
+
 // Show the front page
-func root(w http.ResponseWriter, r *http.Request) *appError {
-	p, err := setup(w, r)
-	if err != nil {
-		return &appError{err, "Could not initialize.", 500}
-	}
+func root(w http.ResponseWriter, r *http.Request) {
+	p := setup(w, r)
+	
 	p.Content = fmt.Sprintf("%s", "<h1>TrypUp: travel, democratized</h1>")
 	t, err := template.ParseFiles("root/view/index.html")
-	if err != nil {
-		return &appError{err, "Could not parse template.", 500}
-	}
+	check(err, "Could not parse template.")
+	
 	t.Execute(w, p)
-	return nil
 }
 
 // Make sure we're ready to go, with Content-Type and more
-func setup(w http.ResponseWriter, r *http.Request) (*Page, error) {
+func setup(w http.ResponseWriter, r *http.Request) *Page {
 	w.Header().Set("Content-Type", "text/html")
-	return &Page{Title: "TrypUp: travel, democratized", Content: ""}, nil
+	return &Page{Title: "TrypUp: travel, democratized", Content: ""}
 }
